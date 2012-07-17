816de3d6a0d583ae1c6579da65cd9b1bda4b9a36
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-07-18 00:12:18 +0300

    Add missing test name -_-

--- test/runner.js
+++ test/runner.js
@@ -1,23 +1,20 @@
-var mocha
 var expect = require('chai').expect;
 var mysql = require('mysql');
+var db;
 
-
-suite('DB Connection', function(){
-
-
-
-
-    test(function(){
+suite('Database', function(){
+    setup(function(){
         var db = mysql.createClient({
             user: 'root',
             password: '',
             host: 'localhost',
             port: 3306
         });
+   });
 
+    test("Connect DB", function(){
         db.query('USE ' + 'ambrosia_test', function(err){
-            expect(err).to.be.null;
+            expect(err).not.to.be.null;
         });
     });
 });
