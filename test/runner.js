14df728349b1d84c18950e65b927305c69c0bb75
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-07-17 22:56:23 +0300

    Try to create a test for db connection

--- test/runner.js
+++ test/runner.js
@@ -1,1 +1,13 @@
-var mocha = require('mocha');
+var expect = require('chai').expect;
+var mysql = require('mysql');
+
+var db = mysql.createClient({
+    user: 'root',
+    password: '',
+    host: 'localhost',
+    port: 3306
+});
+
+db.query('USE ' + 'ambrosia_test', function(err){
+    expect(err).to.equal(null);
+});
