8c2c3fde5699e4343441cfbd109977486a505031
Jari Voutilainen <jari.voutilainen@iki.fi>
2012-07-18 00:02:11 +0300

    Update db test again

--- test/runner.js
+++ test/runner.js
@@ -1,18 +1,21 @@
+var mocha
 var expect = require('chai').expect;
 var mysql = require('mysql');
-var db;
+
 
 suite('DB Connection', function(){
-    setup(function(){
+
+
+
+
+    test(function(){
         var db = mysql.createClient({
             user: 'root',
             password: '',
             host: 'localhost',
             port: 3306
         });
-   });
 
-    test(function(){
         db.query('USE ' + 'ambrosia_test', function(err){
             expect(err).to.be.null;
         });
